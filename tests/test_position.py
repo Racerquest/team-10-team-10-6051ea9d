1d75e2915db64c5999da89b41342647d1c2284f4
Bryan Juchter <mthmzeph4u@gmail.com>
2023-09-21 21:00:23 +0000

    position

--- tests/test_position.py
+++ tests/test_position.py
@@ -1,1 +1,11 @@
+from unittest import TestCase
+from src.levelup.position import Position
+
+class TestPositionInit(TestCase):
+    def test_init(self):
+        xCoordinates = 1
+        yCoordinates = ''
+        testobj = Position(xCoordinates, yCoordinates)
+        self.assertNotEqual(testobj.xCoordinates,"")
+        self.assertNotEqual(testobj.yCoordinates,"")  
 
