13afd4bc4b8aaf00868eb30c361e4d4cf63dbdb0
Donna Washington <dmwashton59@gmail.com>
2023-09-21 15:17:36 +0000

    first test

---
+++ test10_character.py
@@ -0,0 +1,12 @@
+import unittest
+from character import character
+
+
+
+class testcharacter(unittest.TestCase):
+
+  def test_initialization(self):
+        testobj = character()
+        expected_name_value is notnull;
+
+        self.assertIsNone(testobj.value)
