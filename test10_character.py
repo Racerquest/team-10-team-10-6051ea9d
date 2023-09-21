02d046ba1283bfeeb49084c1f18ed09a212de3b1
Donna Washington <dmwashton59@gmail.com>
2023-09-21 15:35:06 +0000

    test2

--- test10_character.py
+++ test10_character.py
@@ -7,6 +7,10 @@ class testcharacter(unittest.TestCase):
 
   def test_initialization(self):
         testobj = character()
-        expected_name_value is notnull;
+ #       expected_name_value is notnull
 
         self.assertIsNone(testobj.value)
+
+
+if __name__ == '__main__':
+    unittest.main()
