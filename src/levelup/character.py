6785d08f5bbfcbc80cd5bf3f92d59f8cc00a0691
Bryan Juchter <mthmzeph4u@gmail.com>
2023-09-21 20:20:33 +0000

    Character Test Finished

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -2,4 +2,6 @@ class Character:
     name = ""
 
     def __init__(self, character_name):
+        if character_name == '':
+            character_name = "bobZilla"
         self.name = character_name
