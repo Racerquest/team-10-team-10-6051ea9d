f1c51126634b28820bbe90e3c10901edb541fd2e
Todd <87922385+Racerquest@users.noreply.github.com>
2023-09-21 14:07:32 -0400

    Create test10_gamemap.py

---
+++ tests/test10_gamemap.py
@@ -0,0 +1,7 @@
+class gamemap(unittest.TestCase):
+
+  def test_initialization(self):
+        testobj = character()
+ #       expected_name_value is notnull
+        
+        self.assertNotEqual(testobj.name,'')
