b35a149cf1bc98de82c67708f1d8fe2c9d5e3d6e
Bryan Juchter <mthmzeph4u@gmail.com>
2023-09-22 13:39:52 +0000

    position

--- src/levelup/position.py
+++ src/levelup/position.py
@@ -3,9 +3,9 @@ class Position:
     yCoordinates = 0
 
     def __init__(self, xCoordinates, yCoordinates):
-#        if xCoordinates or yCoordinates == '':
-#            xCoordinates = 0
-#            yCoordinates = 0
+        if xCoordinates or yCoordinates == '':
+            xCoordinates = 0
+            yCoordinates = 0
         self.xCoordinates = xCoordinates
         self.yCoordinates = yCoordinates
 
