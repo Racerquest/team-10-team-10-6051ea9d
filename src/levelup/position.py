cedc7d45ca14cc1b3cebe252ff9c0755ab283c69
Bryan Juchter <mthmzeph4u@gmail.com>
2023-09-22 15:05:00 +0000

    position

--- src/levelup/position.py
+++ src/levelup/position.py
@@ -1,12 +1,13 @@
 class Position:
-    xCoordinates = 0
-    yCoordinates = 0
+    xyCoordinates = tuple()
+    #yCoordinates = 0
 
-    def __init__(self, xCoordinates, yCoordinates):
-        if xCoordinates or yCoordinates == '':
-            xCoordinates = 0
-            yCoordinates = 0
-        self.xCoordinates = xCoordinates
-        self.yCoordinates = yCoordinates
+    def __init__(self, xyCoordinates):
+        if len(xyCoordinates) != 2:
+            raise Exception("Invalid coordinates")
+         #   xCoordinates = 0
+         #  yCoordinates = 0
+        self.xyCoordinates = xyCoordinates
+        #self.yCoordinates = yCoordinates
 
 
--- tests/test_position.py
+++ tests/test_position.py
@@ -3,9 +3,16 @@ from src.levelup.position import Position
 
 class TestPositionInit(TestCase):
     def test_init(self):
-        xCoordinates = 1
-        yCoordinates = ''
-        testobj = Position(xCoordinates, yCoordinates)
-        self.assertNotEqual(testobj.xCoordinates,"")
-        self.assertNotEqual(testobj.yCoordinates,"")  
+        xycoordinates = (1,1)
+        #xCoordinates = 1
+        #yCoordinates = ''
+        testobj = Position(xycoordinates)
+        self.assertNotEqual(testobj.xyCoordinates[0],"")
+        self.assertNotEqual(testobj.xyCoordinates[1],"") 
+    def test_xylen(self):
+        xycoordinates = (1,5)
+        #xCoordinates = 1
+        #yCoordinates = ''
+        testobj = Position(xycoordinates)
+        self.assertEqual(len(testobj.xyCoordinates),2) 
 
